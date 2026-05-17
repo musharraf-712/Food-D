269cde6bd2e6dc77afa157939dad7d88c885e04f
Musharraf-712 <musharrafsiddiqui712@gmail.com>
2026-05-17 22:43:25 +0530

    update admin

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -18,7 +18,7 @@ const App = () => {
         <Sidebar/>
 
         <Routes>
-          <Route path='/add' element={<Add url={url}/>}/>
+          <Route path='/' element={<Add url={url}/>}/>
           <Route path='/list' element={<List url={url} />}/>
           <Route path='/orders' element={<Orders url={url}/>}/>
         </Routes>
